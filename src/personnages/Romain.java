89a7bd2e6438300c46f5e4631567e83ba6abf75e
Foxier29 <loicb2006@outlook.com>
2025-09-24 15:24:06 +0200

    création de la classe Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,21 +1,21 @@
 package personnages;
 
-public class Gaulois {
+public class Romain {
 	private String nom;
 	private int force;
-	
-	public Gaulois(String nom, int force) {
+	public Romain(String nom, int force) {
 		this.nom = nom;
-		this.force = force;}
+		this.force = force;
+	
+	}
 	public String getNom() {
 		return nom;
 	}
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-
+		System.out.println(prendreParole() +"\"" + texte +"\"");
 	}
 	private String prendreParole() {
-		return "Le gaulois" + nom + " : ";
+		return "Le romain " + nom + ": ";
 	}
 	
 }
